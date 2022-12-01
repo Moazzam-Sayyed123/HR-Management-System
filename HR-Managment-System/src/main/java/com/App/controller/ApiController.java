af5319689171217228088feb5b1f41bf7ef65c8c
Moazzam.Sayyed <Moazzam.Sayyed@opusconsulting.com>
2022-12-01 15:54:04 +0530

    Added UI for Login and dashboard

--- HR-Managment-System/src/main/java/com/App/controller/ApiController.java
+++ HR-Managment-System/src/main/java/com/App/controller/ApiController.java
@@ -1,5 +1,21 @@
 package com.App.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ApiController {
 
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "index.html";
+	}
+	
+	@RequestMapping("/dashboard")
+	public String mainMenu()
+	{
+		return "dashboard.html";
+	}
 }
